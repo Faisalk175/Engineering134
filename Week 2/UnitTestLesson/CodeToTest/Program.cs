cee4a7b635138b82b60429a11f1284c1c6748e1e
Faisalk175 <faisalk3412@gmail.com>
2022-12-06 15:51:11 +0000

    Updated CodeToTest

---
+++ Week 2/UnitTestLesson/CodeToTest/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeToTest;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int timeOfDay = 21;
+        Console.WriteLine(GetGreeting(timeOfDay));
+    }
+
+    private static string GetGreeting(int timeOfDay)
+    {
+        string message;
+        if (timeOfDay >= 5 && timeOfDay <= 12)
+        {
+            message = "Good morning!";
+        }
+        else if (timeOfDay >= 12 && timeOfDay <= 18)
+        {
+            message = "Good afternoon!";
+        }
+        else
+        {
+            message = "Good evening!";
+        }
+        return message;
+    }
+}
