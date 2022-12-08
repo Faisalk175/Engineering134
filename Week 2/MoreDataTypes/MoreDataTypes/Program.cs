0d688fe2e916b4be3acf8aa648daa81f376ce500
Faisalk175 <faisalk3412@gmail.com>
2022-12-08 16:02:52 +0000

    inital commit, for MoreDataTypes

---
+++ Week 2/MoreDataTypes/MoreDataTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MoreDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = " C# list fundamentals";
+            Console.WriteLine(StringExercise(myString));
+        }
+
+        public static string StringExercise(string myString)
+        {
+            return myString.Replace(" ", "").ToUpper().Replace('L', '*').Replace('T', '*');
+        }
+    }
+
+}
+
+
+
+
+
+
