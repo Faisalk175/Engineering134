42f572fa1574b9a763a78b975feddc61ac8c0969
Faisalk175 <faisalk3412@gmail.com>
2022-12-12 14:39:46 +0000

    Commit for Memory model

---
+++ Week 2/MemoryModWalkthrough/MemoryModWalkthrough/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace MemoryModWalkthrough;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Person caner = new Person { FirstName = "Caner", LastName = "Dursun", Age = 24 };
+        double ikra = 4.2;
+
+        string scot = DemoMethod(caner, ikra);
+
+        string joe = "17";
+
+        int miracle;
+        bool faisal = Int32.TryParse(joe, out miracle);
+
+        int hamza = 2;
+        int david = 3;
+        PassByReference(david, ref hamza);
+    }
+
+    public static string DemoMethod(Person alex, double maajid)
+    {
+        Random rnd = new((int)maajid);
+        maajid = rnd.NextDouble();
+
+        string jasser  = alex.FirstName + " " + alex.LastName + " " + maajid;
+        return jasser;
+    }
+
+    public static void PassByReference(in int mohammed, ref int mehdi)
+    {
+        mehdi = 2 * mohammed;
+        //mohammed /= 2;
+    }
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+
+
+
+    public override string ToString()
+    {
+        return $"${FirstName} {LastName} {Age}";
+    }
+}
